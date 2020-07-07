ba1f1e6c2f4acbf6276b082e49bccd0728b8ca02
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-08 01:51:00 +0200

    app js

--- js/app.js
+++ js/app.js
@@ -5,25 +5,62 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65 */
 
 
-var lunghezzaViaggio=parseInt(prompt('Quanti km devi percorrere?'));
-var etaViaggiatori=parseInt(prompt('Quanti anni hai?'))
-var prezzoKm=0.21*lunghezzaViaggio;
+
+// metodo 1
+/* var lunghezzaViaggio = parseInt(prompt('Quanti km devi percorrere?'));
+var etaViaggiatori = parseInt(prompt('Quanti anni hai?'));
+var prezzoKm = 0.21 * lunghezzaViaggio;
 var scontoSulPrezzoMinori;
 var scontoSulPrezzoOver;
 
+
+
 if(etaViaggiatori < 18){
-    var scontoSulPrezzoMinori= ( 20 * prezzoKm / 100 );
+    var scontoSulPrezzoMinori = ( 20 * prezzoKm / 100 );
     var prezzoFinale = prezzoKm - scontoSulPrezzoMinori;
 }
 
 else if(etaViaggiatori >= 65){
-    var scontoSulPrezzoOver= ( 40 * prezzoKm / 100 );
-    var prezzoFinale= prezzoKm - scontoSulPrezzoOver;
+    var scontoSulPrezzoOver = ( 40 * prezzoKm / 100 );
+    var prezzoFinale = prezzoKm - scontoSulPrezzoOver;
 }
 
 else{
-    var prezzoFinale=lunghezzaViaggio;
+    var prezzoFinale = prezzoKm;
 }
 
 
-var risultato= document.getElementById('risultato').innerHTML='il tuo biglietto costa '+ prezzoFinale + ' euro';
+var risultato= document.getElementById('risultato').innerHTML='il tuo biglietto costa ' +  Math.round(prezzoFinale) + ' euro'; 
+ */
+
+
+
+
+// metodo 2
+
+var  prezzoKm;
+var lunghezza;
+var eta;
+
+function Viaggio(){
+    var lunghezza=parseInt(prompt('Quanti km devi percorrere?'));
+    var eta=parseInt(prompt('Quanti anni hai?'));
+    var prezzoKm= 0.21 * lunghezza;
+    if(eta < 18){
+      var prezzoKm= ( prezzoKm * 20 / 100 ) - prezzoKm;
+      var sconto18 = prezzoKm;
+      return 'Hai '+ eta + ' anni il prezzo del tuo biglietto è di ' + sconto18 + ' euro per la distanza di ' + lunghezza + ' km';
+    }
+    else if(eta >= 65){
+        var prezzoKm= ( prezzoKm * 40 / 100 ) - prezzoKm;
+        var sconto65= prezzoKm;
+        return 'Hai '+ eta + ' anni il prezzo del tuo biglietto è di ' + sconto65 + ' euro per la distanza di ' + lunghezza + ' km';
+    }
+    else{
+        return 'Hai '+ eta + ' anni il prezzo del tuo biglietto è di ' + prezzoKm + ' euro per la distanza di ' + lunghezza + ' km';
+    }
+    
+}
+
+
+console.log(Viaggio())
